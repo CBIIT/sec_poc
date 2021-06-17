259be17f9fe4f8cfab89e92ffb57722748b95f46
Hubert Hickman <hickmanhb@nih.gov>
2021-06-17 10:33:24 -0500

    Backport for geolocation fix

--- get_lat_long_for_zipcode.R
+++ get_lat_long_for_zipcode.R
@@ -3,20 +3,23 @@ library(httr)
 get_lat_long_for_zipcode  <- function(zipcode) {
   #https://public.opendatasoft.com/api/records/1.0/search/?dataset=us-zip-code-latitude-and-longitude&q=80302&facet=state&facet=timezone&facet=dst
   
+  # https://public.opendatasoft.com/api/records/1.0/search/?dataset=georef-united-states-of-america-zc-point&q=71016&
+  
+  
   d <-
     GET(
       "https://public.opendatasoft.com/api/records/1.0/search",
-      query = list(dataset = "us-zip-code-latitude-and-longitude", q = zipcode)
+      query = list(dataset = "georef-united-states-of-america-zc-point", q = zipcode)
     )
   
   pdata <- content(d)
   print(pdata$nhits)
   if (pdata$nhits == 1) {
     return(c(
-      pdata$records[[1]]$fields$latitude,
-      pdata$records[[1]]$fields$longitude,
-      pdata$records[[1]]$fields$city,
-      pdata$records[[1]]$fields$state
+      pdata$records[[1]]$fields$geo_point_2d[[1]],
+      pdata$records[[1]]$fields$geo_point_2d[[2]],
+      pdata$records[[1]]$fields$usps_city,
+      pdata$records[[1]]$fields$stusps_code
     ))
   } else {
     return(pdata$nhits)
