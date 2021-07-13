d94c8c36e60ba5f404e3130c16273bd651853ddb
Hubert Hickman <hickmanhb@nih.gov>
2021-07-13 08:31:32 -0500

    Added CTS API V2 matching for diseases (if env var is set).

--- get_api_studies_for_disease.R
+++ get_api_studies_for_disease.R
@@ -3,27 +3,9 @@
 #
 get_api_studies_for_disease  <- function(ncit_code) {
   start <- 0
-  
-  d <-
-    POST(
-      "https://clinicaltrialsapi.cancer.gov/v1/clinical-trials",
-      body = list(
-        current_trial_status = 'active',
-        primary_purpose.primary_purpose_code = c('treatment','screening'),
-        diseases.nci_thesaurus_concept_id = ncit_code,
-        sites.recruitment_status = 'ACTIVE',
-        include = list('nct_id'),
-        size = 50,
-        from = start
-      ),
-      encode = "json"
-    )
-  pdata <- content(d)
-  total_to_return <- pdata$total
-  df <- rbindlist(pdata$trials)
-  num_returned <- nrow(df)
-  start <- num_returned
-  while(start < total_to_return) {
+  CTS_V2_API_KEY <- Sys.getenv('CTS_V2_API_KEY')
+  if (nchar(CTS_V2_API_KEY) == 0) {
+    # V1
     d <-
       POST(
         "https://clinicaltrialsapi.cancer.gov/v1/clinical-trials",
@@ -39,11 +21,79 @@ get_api_studies_for_disease  <- function(ncit_code) {
         encode = "json"
       )
     pdata <- content(d)
-    dfi <- rbindlist(pdata$trials) # Turns the list into a dataframe
-    start <- start + nrow(dfi)
-    df <- rbind(df, dfi)
+    total_to_return <- pdata$total
     
+    df <- rbindlist(pdata$trials)
+    num_returned <- nrow(df)
+    start <- num_returned
+    while (start < total_to_return) {
+      d <-
+        POST(
+          "https://clinicaltrialsapi.cancer.gov/v1/clinical-trials",
+          body = list(
+            current_trial_status = 'active',
+            primary_purpose.primary_purpose_code = c('treatment', 'screening'),
+            diseases.nci_thesaurus_concept_id = ncit_code,
+            sites.recruitment_status = 'ACTIVE',
+            include = list('nct_id'),
+            size = 50,
+            from = start
+          ),
+          encode = "json"
+        )
+      pdata <- content(d)
+      dfi <- rbindlist(pdata$trials) # Turns the list into a dataframe
+      start <- start + nrow(dfi)
+      df <- rbind(df, dfi)
+      
+    }
+    # print(df)
+    return(df)
+  } else {
+    # V2
+    d <-
+      POST(
+        'https://clinicaltrialsapi.cancer.gov/api/v2/trials',
+        body = list(
+          current_trial_status = 'Active',
+          primary_purpose = c('TREATMENT', 'SCREENING'),
+          diseases.nci_thesaurus_concept_id = ncit_code,
+          sites.recruitment_status = 'ACTIVE',
+          include = list('nct_id'),
+          size = 50,
+          from = 0
+        ),
+        encode = "json",
+        add_headers(`x-api-key` = CTS_V2_API_KEY, `Content-Type` = 'application/json')
+      )
+    pdata <- content(d)
+    total_to_return <- pdata$total
+    
+    df <- rbindlist(pdata$data)
+    num_returned <- nrow(df)
+    start <- num_returned
+    while (start < total_to_return) {
+      d <-
+        POST(
+          'https://clinicaltrialsapi.cancer.gov/api/v2/trials',
+          body = list(
+            current_trial_status = 'Active',
+            primary_purpose = c('TREATMENT', 'SCREENING'),
+            diseases.nci_thesaurus_concept_id = ncit_code,
+            sites.recruitment_status = 'ACTIVE',
+            include = list('nct_id'),
+            size = 50,
+            from = start
+          ),
+          encode = "json",
+          add_headers(`x-api-key` = CTS_V2_API_KEY, `Content-Type` = 'application/json')
+        )
+      pdata <- content(d)
+      dfi <- rbindlist(pdata$data) # Turns the list into a dataframe
+      start <- start + nrow(dfi)
+      df <- rbind(df, dfi)
+      
+    }
+    return(df)
   }
-  # print(df)
-  return(df)
 }
