64980c816667c8b51212687495ad4f2fad67ed8b
Hubert Hickman <hickmanhb@nih.gov>
2021-11-10 11:18:15 -0600

    Switched to bing maps for geolocation API that actually works with leading zeroes

--- get_lat_long_for_zipcode.R
+++ get_lat_long_for_zipcode.R
@@ -1,6 +1,9 @@
 library(httr)
 
+
 get_lat_long_for_zipcode  <- function(zipcode) {
+  dbinfo <- config::get()
+  print(zipcode)
   #https://public.opendatasoft.com/api/records/1.0/search/?dataset=us-zip-code-latitude-and-longitude&q=80302&facet=state&facet=timezone&facet=dst
   
   # https://public.opendatasoft.com/api/records/1.0/search/?dataset=georef-united-states-of-america-zc-point&q=71016&
@@ -8,20 +11,26 @@ get_lat_long_for_zipcode  <- function(zipcode) {
   
   d <-
     GET(
-      "https://public.opendatasoft.com/api/records/1.0/search",
-      query = list(dataset = "georef-united-states-of-america-zc-point", q = zipcode)
+      "https://dev.virtualearth.net/REST/v1/Locations",
+      query = list(CountryRegion = 'US', 
+                   q = toString(zipcode),
+                   maxResults =1 ,
+                   key =  dbinfo$bing_maps_api_key,
+                   postalCode = zipcode)
     )
   
   pdata <- content(d)
-  print(pdata$nhits)
-  if (pdata$nhits == 1) {
+  #print(pdata)
+  if (pdata$resourceSets[[1]]$estimatedTotal == 1) {
     return(c(
-      pdata$records[[1]]$fields$geo_point_2d[[1]],
-      pdata$records[[1]]$fields$geo_point_2d[[2]],
-      pdata$records[[1]]$fields$usps_city,
-      pdata$records[[1]]$fields$stusps_code
+      pdata$resourceSets[[1]]$resources[[1]]$geocodePoints[[1]]$coordinates[[1]],
+      pdata$resourceSets[[1]]$resources[[1]]$geocodePoints[[1]]$coordinates[[2]],
+      #pdata$records[[1]]$fields$usps_city,
+      pdata$resourceSets[[1]]$resources[[1]]$address$locality,
+      #pdata$records[[1]]$fields$stusps_code
+      pdata$resourceSets[[1]]$resources[[1]]$address$adminDistrict
     ))
   } else {
-    return(pdata$nhits)
+    return(pdata$resourceSets[[1]]$estimatedTotals)
   }
 }
