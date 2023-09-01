fc7f938742143ffb914db470a494456a80bc0488
John Callaway <john.callaway@.nih.gov>
2023-09-01 13:49:21 -0400

    Add a couple more convenience packages, and forward shiny port.

--- docker/R_3.6.3/build_and_run.sh
+++ docker/R_3.6.3/build_and_run.sh
@@ -7,4 +7,4 @@ docker build -t sec_poc/r .
 # Uncomment the following line and modify the first part of the -v argument to
 # point to where your sec_poc repo is checked out.  Then that directory will
 # be available in the container based on the second part of the argument.
-#docker run -v ~/Dev/CTRP/sec_poc:/opt/R/sec_poc sec_poc/r
+#docker run -v ~/Dev/CTRP/sec_poc:/opt/R/sec_poc -p 8888:8888 sec_poc/r
