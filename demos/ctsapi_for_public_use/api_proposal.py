311005ea02ac7d8d9f5e0219afbe5c0ada66f491
crouchcd <cameroncrouch36@gmail.com>
2025-08-04 20:07:10 +0000

    add commentary to api_proposal

--- demos/ctsapi_for_public_use/api_proposal.py
+++ demos/ctsapi_for_public_use/api_proposal.py
@@ -11,9 +11,13 @@ study_source = st.selectbox(
     options=["Externally Peer Reviewed", "Industrial", "Institutional", "National"],
     width=260,
 )
-st.markdown("""
-> Select a study source which we will treat as a "private" field for search.
-""")
+st.info(
+    """
+Select a study source which we will treat as a "private" field for search.
+""",
+    icon=":material/info:",
+    width=500,
+)
 
 
 @st.cache_data
@@ -57,8 +61,8 @@ body = {{
         "current_trial_status",
         "nci_id",
         "nct_id",
-        // study_source is actually included, but we can pretend that it's not.
-        // This implementation would need to be handled by the CTS API team.
+        "study_source" // <- Even if study_source is requested, the API should not return it.
+                       //    This will need to be implemented by the CTS API team. 
     ],
     "current_trial_status": [
         "Active",
@@ -70,7 +74,7 @@ body = {{
     ],
     "from": 0,
     "size": 50,
-    "study_source": "{study_source}",
+    "study_source": "{study_source}", // <- Here we query on a field that is "private"; it's for searching only
 }}
 requests.post(
     "https://clinicaltrialsapi.cancer.gov/api/v2/trials",
@@ -82,13 +86,15 @@ requests.post(
 
 if st.button("Search", disabled=not study_source):
     trials, total = query_ctsapi(study_source)
-    st.write(f"Total Trials Found: {total}")
+    st.write("Total Trials Found:", total)
     st.write("Example Response:")
     for trial in trials:
         assert trial["study_source"] == study_source, "Study source mismatch"
     del trial["study_source"]  # Remove study_source for display
     trial["matched_on"] = {"study_source": study_source}
     st.json(trial)
-    st.markdown("""
-> Note: The `study_source` field is not included in the response because it is treated as a private field for searching only.
-""")
+    st.info(
+        "The study_source field is not included in the response because it is treated as a private field for searching only.",
+        icon=":material/info:",
+        width=500,
+    )
